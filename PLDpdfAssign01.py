4b9945f93cd0cca44847d81799e43ea9edb2ef8f
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2022-02-10 14:42:28 +0800

    Intro prog

---
+++ PLDpdfAssign01.py
@@ -0,0 +1,6 @@
+# Program for PDF Resume Creator
+
+#Wodnerfful Assignment 9 PDF Resume Creator - Create a python program that will create your personal resume in
+#  PDF format - All personal details are stored in a JSON file - Your program should read the JSON file and write
+#  the details in the PDF - The output file should be: LASTNAME_FIRSTNAME.pdf Note: Cool Proggramming easy
+# Wonderful+ WOnderful
