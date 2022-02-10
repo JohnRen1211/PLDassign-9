0812e171a338d693bbb2156033a3823854f33d44
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2022-02-10 14:53:04 +0800

    Making pdf line

--- PLDpdfAssign01.py
+++ PLDpdfAssign01.py
@@ -14,10 +14,18 @@ from fpdf import FPDF
 # Format ('A3', 'A4' (default), 'A5', 'Letter', 'Legal', (100, 150))
 pdf = FPDF('P', 'mm', 'Letter')
 
-# Add page 
+# Add page
 pdf.add_page()
 
 # Selecting specific font
 #fonts ('times', 'courier', 'helvetica', 'symbol', 'zpfdingbats,')
 # 'B' (bold), 'U' (underline), 'I' (italics), '' (regular), combination (i.e., (BU)) 
 pdf.set_font('helvetica', '', 16)
+
+#Add text
+# w = width
+# h = height
+
+pdf.cell(40, 10, 'Wonderful Code')
+pdf.output('pdf_resume.pdfpython') 
+
