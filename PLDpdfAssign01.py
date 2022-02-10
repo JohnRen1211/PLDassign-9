f7d8d6637cb7515faa3e9d4e5404d32514f4ab92
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2022-02-10 14:46:49 +0800

    Creating fpdf object

--- PLDpdfAssign01.py
+++ PLDpdfAssign01.py
@@ -6,3 +6,10 @@
 from fpdf import FPDF
 
 # Import pdf reader 
+
+#create fpdf object
+
+# Layout ('P', 'L')
+# Unit ('mm', 'cm', 'in')
+# Format ('A3', 'A4' (default), 'A5', 'Letter', 'Legal', (100, 150))
+pdf = FPDF('P', 'mm', 'Letter')
